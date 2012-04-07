760861ccf0d9b5ddb5fefc0fb1570a6beee60616
David Snyder <dsnyder90@gmail.com>
2012-04-06 20:03:19 -0500

    editing ballots/create so option create callback is called

--- app/controllers/ballots_controller.rb
+++ app/controllers/ballots_controller.rb
@@ -20,18 +20,25 @@ class BallotsController < ApplicationController
     #poll would benefit from it
     if(klass < PlaceOption)
       #location = location_for(request.remote_ip)
+      #FIXME: hardcoded location
       @ballot.geo_filter = klass.geo_filter_for(30.30,-97.68,APP_CONFIG['factual']['search_radius'])
     end
    end
 
   def create
-    option_klass = params[:ballot][:option_klass]
-    geo_filter   = params[:ballot][:geo_filter].nil? ? {} : Yajl::Parser.parse(params[:ballot][:geo_filter])
+    option_klass   = params[:ballot][:option_klass]
+    geo_filter     = params[:ballot][:geo_filter].nil? ? {} : Yajl::Parser.parse(params[:ballot][:geo_filter])
     search_filters = params[:ballot][:search_filters].nil? ? [] : Yajl::Parser.parse(params[:ballot][:search_filters])
 
-    params[:ballot][:options_attributes] = (option_klass.constantize).search('sandwich',geo_filter,search_filters,params[:page]||1)["response"]["data"][0..3]
-
     @ballot = Ballot.new(params[:ballot])
+
+    #TODO: defaulting to 4 static options
+    options_attributes = (option_klass.constantize).search('sandwich',geo_filter,search_filters,params[:page]||1)["response"]["data"][0..3]
+    options_attributes.each do |option_attrs|
+      @option = (@ballot.option_klass.constantize).new(option_attrs)
+      @option.ballot = @ballot
+      @option.save
+    end
 
     if(@ballot.save)
       redirect_to ballot_path(@ballot.uuid)
@@ -42,9 +49,10 @@ class BallotsController < ApplicationController
 
   def update
     @ballot = Ballot.find(params[:id])
-    @option = (@ballot.option_klass.constantize).new(params[:ballot][:options_attributes])
-    @ballot.options << @option
+    @ballot.options << (@ballot.option_klass.constantize).new(params[:ballot][:options_attributes])
+
     if(@ballot.save)
+      #FIXME: hardcoded channel
       redis_client.publish('dl.channel.votes',Yajl::Encoder.encode(@ballot))
       respond_to do |format|
         format.json { render :json => Yajl::Encoder.encode(@ballot) }
@@ -72,6 +80,7 @@ class BallotsController < ApplicationController
     return @response if @response
     ip = (ip.eql?("127.0.0.1")) ? "76.253.73.79" : ip #can't test locally...
     Chimps.config[:query][:key] = APP_CONFIG['infochimps']['apikey']
+    #FIXME: hardcoded query path
     @response ||= Chimps::QueryRequest.new('web/analytics/ip_mapping/digital_element/geo', :query_params => { :ip => ip } ).get.parse!.data
   end
 
